773550a8a19c911bb2e52be30311eeebf387cfc1
Ryan <43714844+Xiaowei1102@users.noreply.github.com>
2023-07-27 01:43:28 -0500

    add evenlistener to change color when hover, clear old grid and generate new grid

--- javascript.js
+++ javascript.js
@@ -1,23 +1,48 @@
-for (let i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-    div.classList.add('container');
-    document.body.appendChild(div);
-}
-
-const divs = document.querySelectorAll('.container');
-
-divs.forEach(div => {
-    for (let i = 0; i < 16; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        div.appendChild(square);
+function generateSquareGrid(grideNumber) {
+    for (let i = 0; i < grideNumber; i++) {
+        const div = document.createElement('div');
+        div.classList.add('container');
+        document.body.appendChild(div);
     }
-})
+    
+    const divs = document.querySelectorAll('.container');
+    
+    divs.forEach(div => {
+        for (let i = 0; i < grideNumber; i++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            div.appendChild(square);
+        }
+    })
+}
+generateSquareGrid(16);
 
 //add evetlistener for each element when the mouse is hovering over
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => square.addEventListener(
+function sketch() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.addEventListener(
     'mouseenter', 
     (e) => {
         e.target.style.backgroundColor = 'purple';
     }));
+}
+
+sketch();
+
+
+//add button eventlistener to clear the grids and generate new ones
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    let newGridSquareNum = prompt("Choose the new grid square number on each side of the screen");
+    newGridSquareNum = parseInt(newGridSquareNum, 10);
+
+    console.log(newGridSquareNum);
+    //clear the old grid
+    const elements = document.querySelectorAll('div');
+    elements.forEach(element => element.remove());
+
+    //reconstruct the new grid
+    generateSquareGrid(newGridSquareNum);
+    sketch();
+
+});
